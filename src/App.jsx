224885e4e71585f8f63c3704af9b92b0df74e723
Ilya <bondarenkoIlyaProfession@gmail.com>
2023-01-26 15:54:37 +0300

    Set Up the Project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./scss/app.scss";
+
+export const App = () => {
+  return (
+    <div className="wrapper">
+      <div className="content" />
+    </div>
+  );
+};
