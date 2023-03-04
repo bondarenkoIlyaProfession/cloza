3c937b01911de2c24d331843c0492b57cc34f5ed
Ilya <bondarenkoIlyaProfession@gmail.com>
2023-03-05 00:12:39 +0300

    Linted the SCSS Files and Fixed Those

---
+++ src/layout/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import 
+
+export const Home = () => {
+  return (
+    <section className="home">
+      <div className="container">1</div>
+    </section>
+  );
+};
